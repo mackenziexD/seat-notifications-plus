6077846b2f7c90f9f680c5e0345f8410a0725889
Mackenzie James <mackenzie.14@hotmail.co.uk>
2024-03-05 23:12:11 +0000

    Update CharacterNotificationObserver.php

--- src/Observers/CharacterNotificationObserver.php
+++ src/Observers/CharacterNotificationObserver.php
@@ -18,8 +18,8 @@ class CharacterNotificationObserver
         $corporationId = $notification->recipient->affiliation->corporation_id;
         
         $mostRecentSeatNotification = SeatNotificationsPlus::where('corporation_id', $corporationId)
-        ->latest('most_recent_notification')
-        ->first();
+            ->latest('most_recent_notification')
+            ->first();
 
         $isNewNotification = is_null($mostRecentSeatNotification) || $notification->timestamp > $mostRecentSeatNotification->most_recent_notification;
 
