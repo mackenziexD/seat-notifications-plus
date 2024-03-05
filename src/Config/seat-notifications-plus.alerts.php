d98a2937ace0dd11e087d2b7dba55e2eb02eacca
Mackenzie James <mackenzie.14@hotmail.co.uk>
2024-03-05 02:02:00 +0000

    fixing alerts.php and obs

--- src/Config/seat-notifications-plus.alerts.php
+++ src/Config/seat-notifications-plus.alerts.php
@@ -4,127 +4,127 @@ return [
     'AllAnchoringMsg [N+]' => [
         'label' => 'AllAnchoringMsg [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Structures\AllAnchoringMsg::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Structures\AllAnchoringMsg::class
         ],
     ],
     'EntosisCaptureStarted [N+]' => [
         'label' => 'EntosisCaptureStarted [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Sovereignties\EntosisCaptureStarted::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Sovereignties\EntosisCaptureStarted::class
         ],
     ],
     'MoonMiningExtractionFinished [N+]' => [
         'label' => 'MoonMiningExtractionFinished [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Structures\MoonMiningExtractionFinished::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Structures\MoonMiningExtractionFinished::class
         ],
     ],
     'MoonMiningExtractionStarted [N+]' => [
         'label' => 'MoonMiningExtractionStarted [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Structures\MoonMiningExtractionStarted::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Structures\MoonMiningExtractionStarted::class
         ],
     ],
     'OrbitalAttacked [N+]' => [
         'label' => 'OrbitalAttacked [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Structures\OrbitalAttacked::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Structures\OrbitalAttacked::class
         ],
     ],
     'OwnershipTransferred [N+]' => [
         'label' => 'OwnershipTransferred [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Structures\OwnershipTransferred::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Structures\OwnershipTransferred::class
         ],
     ],
     'SovCommandNodeEventStarted [N+]' => [
         'label' => 'SovCommandNodeEventStarted [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Sovereignties\SovCommandNodeEventStarted::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Sovereignties\SovCommandNodeEventStarted::class
         ],
     ],
     'SovStructureDestroyed [N+]' => [
         'label' => 'SovStructureDestroyed [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Sovereignties\SovStructureDestroyed::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Sovereignties\SovStructureDestroyed::class
         ],
     ],
     'SovStructureReinforced [N+]' => [
         'label' => 'SovStructureReinforced [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Sovereignties\SovStructureReinforced::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Sovereignties\SovStructureReinforced::class
         ],
     ],
     'StructureAnchoring [N+]' => [
         'label' => 'StructureAnchoring [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureAnchoring::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureAnchoring::class
         ],
     ],
     'StructureDestroyed [N+]' => [
         'label' => 'StructureDestroyed [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureDestroyed::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureDestroyed::class
         ],
     ],
     'StructureFuelAlert [N+]' => [
         'label' => 'StructureFuelAlert [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureFuelAlert::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureFuelAlert::class
         ],
     ],
     'StructureLostArmor [N+]' => [
         'label' => 'StructureLostArmor [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureLostArmor::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureLostArmor::class
         ],
     ],
     'StructureLostShields [N+]' => [
         'label' => 'StructureLostShields [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureLostShields::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureLostShields::class
         ],
     ],
     'StructureServicesOffline [N+]' => [
         'label' => 'StructureServicesOffline [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureServicesOffline::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureServicesOffline::class
         ],
     ],
     'StructureUnanchoring [N+]' => [
         'label' => 'StructureUnanchoring [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureUnanchoring::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureUnanchoring::class
         ],
     ],
     'StructureUnderAttack [N+]' => [
         'label' => 'StructureUnderAttack [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureUnderAttack::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureUnderAttack::class
         ],
     ],
     'StructureWentHighPower [N+]' => [
         'label' => 'StructureWentHighPower [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureWentHighPower::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureWentHighPower::class
         ],
     ],
     'StructureWentLowPower [N+]' => [
         'label' => 'StructureWentLowPower [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureWentLowPower::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Structures\StructureWentLowPower::class
         ],
     ],
     'TowerResourceAlertMsg [N+]' => [
         'label' => 'TowerResourceAlertMsg [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Towers\TowerResourceAlertMsg::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Towers\TowerResourceAlertMsg::class
         ],
     ],
     'TowerAlertMsg [N+]' => [
         'label' => 'TowerAlertMsg [N+]',
         'handlers' => [
-            'discord' => \Helious\SeatNotificationsPlus\Notifications\Towers\TowerAlertMsg::class
+            'slack' => \Helious\SeatNotificationsPlus\Notifications\Towers\TowerAlertMsg::class
         ],
     ]
 ];
--- src/Observers/CharacterNotificationObserver.php
+++ src/Observers/CharacterNotificationObserver.php
@@ -16,25 +16,20 @@ class CharacterNotificationObserver
         \Log::error('Notification created');
 
         $corporationId = $notification->recipient->affiliation->corporation_id;
-        // Attempt to retrieve the most recent notification for the corporation
+        
         $mostRecentSeatNotification = SeatNotificationsPlus::where('corporation_id', $corporationId)
-                                                            ->latest('most_recent_notification')
-                                                            ->first();
+        ->latest('most_recent_notification')
+        ->first();
 
         $isNewNotification = is_null($mostRecentSeatNotification) || $notification->timestamp > $mostRecentSeatNotification->most_recent_notification;
 
         // If there is no record or if the current notification is newer, update or create the record
-        if ($isNewNotification) {
-            SeatNotificationsPlus::updateOrCreate(
-                ['corporation_id' => $corporationId],
-                ['most_recent_notification' => $notification->timestamp]
-            );
-            $this->dispatch($notification);
-        } else {
-            \Log::error($notification->timestamp .$mostRecentSeatNotification->most_recent_notification);
-            \Log::error('Notification is not new, not dispatching');
-            return;
-        }
+        if (!$isNewNotification) return;
+        SeatNotificationsPlus::updateOrCreate(
+            ['corporation_id' => $corporationId],
+            ['most_recent_notification' => $notification->timestamp]
+        );
+        $this->dispatch($notification);
     }
 
     /**
@@ -44,20 +39,76 @@ class CharacterNotificationObserver
      */
     private function dispatch(CharacterNotification $notification)
     {
-        $groups = NotificationGroup::with('alerts', 'affiliations')
+        // detect handlers setup for the current notification
+        $handlers = config(sprintf('notifications.alerts.%s.handlers', $notification->type), []);
+
+        // if the notification is unsupported (no handlers available), log and interrupt
+        if (empty($handlers)) {
+            logger()->debug('Unsupported notification type.', [
+                'type' => $notification->type,
+                'sender_type' => $notification->sender_type,
+                'notification' => $notification->text,
+            ]);
+
+            return;
+        }
+
+        // retrieve routing candidates for the current notification
+        $routes = $this->getRoutingCandidates($notification);
+
+        // in case no routing candidates has been delivered, exit
+        if ($routes->isEmpty())
+            return;
+
+        // attempt to enqueue a notification for each routing candidate
+        $routes->each(function ($integration) use ($handlers, $notification) {
+            if (array_key_exists($integration->channel, $handlers)) {
+
+                // extract handler from the list
+                $handler = $handlers[$integration->channel];
+
+                // enqueue the notification
+                Notification::route($integration->channel, $integration->route)
+                    ->notify(new $handler($notification));
+            }
+        });
+    }
+
+    /**
+     * Provide a unique list of notification channels (including driver and route).
+     *
+     * @param  \Seat\Eveapi\Models\Character\CharacterNotification  $notification
+     * @return \Illuminate\Support\Collection
+     */
+    private function getRoutingCandidates(CharacterNotification $notification)
+    {
+        $settings = NotificationGroup::with('alerts', 'affiliations')
             ->whereHas('alerts', function ($query) use ($notification) {
                 $query->where('alert', $notification->type . ' [N+]');
             })->whereHas('affiliations', function ($query) use ($notification) {
                 $query->where('affiliation_id', $notification->character_id);
                 $query->orWhere('affiliation_id', $notification->recipient->affiliation->corporation_id);
             })->get();
+        
+        // loop over each group candidate and collect available integrations
+        $routes = $settings->map(function ($group) {
+            return $group->integrations->map(function ($channel) {
 
-        \Log::error($groups);
+                // extract the route value from settings field
+                $setting = (array) $channel->settings;
+                $key = array_key_first($setting);
+                $route = $setting[$key];
 
-        // Proceed with dispatching the notifications
-        $this->dispatchNotifications($notification->type . ' [N+]', $groups, function ($notificationClass) use ($notification) {
-            \Log::error($notificationClass);
-            return new $notificationClass($notification);
+                // build a composite object build with channel and route
+                return (object) [
+                    'channel' => $channel->type,
+                    'route' => $route,
+                ];
+            });
+        });
+
+        return $routes->flatten()->unique(function ($integration) {
+            return $integration->channel . $integration->route;
         });
     }
     
