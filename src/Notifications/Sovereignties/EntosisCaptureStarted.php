fdc85f80268dd03d9bd72dad1d7928d74a3eb999
Mackenzie James <mackenzie.14@hotmail.co.uk>
2024-03-14 23:59:40 +0000

    add owner of ihub

--- src/Notifications/Sovereignties/EntosisCaptureStarted.php
+++ src/Notifications/Sovereignties/EntosisCaptureStarted.php
@@ -11,6 +11,7 @@ use Seat\Eveapi\Models\Sde\InvType;
 use Seat\Eveapi\Models\Sde\MapDenormalize;
 use Seat\Eveapi\Models\Universe\UniverseName;
 use Seat\Eveapi\Models\Universe\UniverseStructure;
+use Seat\Eveapi\Models\Sovereignty\SovereigntyMap;
 use Seat\Eveapi\Models\Sde\Planet;
 use Seat\Eveapi\Models\Sde\Region;
 use Carbon\Carbon;
@@ -51,13 +52,14 @@ class EntosisCaptureStarted  extends AbstractNotification
             $corpID = $this->notification->recipient->affiliation->corporation_id;
             $system = MapDenormalize::find($this->notification->text['solarSystemID']);
             $region = Region::find($system->regionID)->name;
+            $owner = SovereigntyMap::find($this->notification->text['solarSystemID']) ? SovereigntyMap::find($this->notification->text['solarSystemID'])->alliance->name : 'Unknown';
             $type = InvType::find($this->notification->text['structureTypeID']);
             
             $attachment->color('warning');
             $attachment->author($corpName, '', 'https://images.evetech.net/corporations/'.$corpID.'/logo?size=128');
             $attachment->thumb('https://images.evetech.net/types/'.$type->typeID.'/icon?size=128');
             $attachment->title("{$type->group->groupName} in {$system->itemName} is being captured");
-            $attachment->content("A capsuleer has started to influence the {$type->typeName} in {$this->zKillBoardToDiscordLink('system',$system->itemID,$system->itemName)} ({$region}) with an Entosis Link.");
+            $attachment->content("A capsuleer has started to influence the {$type->typeName} in {$this->zKillBoardToDiscordLink('system',$system->itemID,$system->itemName)} ({$region}) belonging to {$owner} with an Entosis Link.");
             $attachment->timestamp(Carbon::createFromFormat('Y-m-d H:i:s', $this->notification->timestamp));
         });
     }
