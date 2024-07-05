c6347ad8ad85fc0677f443810c46837ab88c8899
Mackenzie James <mackenzie.14@hotmail.co.uk>
2024-07-05 02:41:07 +0100

    Update TestObs.php

--- src/Console/TestObs.php
+++ src/Console/TestObs.php
@@ -3,18 +3,16 @@
 namespace Helious\SeatNotificationsPlus\Console;
 
 use Illuminate\Console\Command;
-use Helious\SeatNotificationsPlus\Models\SeatNotificationsPlus;
 use Seat\Eveapi\Models\Character\CharacterNotification;
 use Carbon\Carbon;
 
 /**
- * Class RemindOperation.
+ * Class TestObs.
  *
- * @package Seat\Kassie\Calendar\Commands
+ * @package Helious\SeatNotificationsPlus\Console
  */
 class TestObs extends Command
 {
-    
     /**
      * @var string
      */
@@ -31,16 +29,15 @@ class TestObs extends Command
     public function handle()
     {
         CharacterNotification::create([
-            'character_id' => 94154296,
-            'notification_id' => 1923730118,
-            'type' => 'MoonminingLaserFired',
+            'character_id' => 2114464253,
+            'notification_id' => 1993510159,
+            'type' => 'unknown notification type (283)',
             'sender_id' => 1000137,
             'sender_type' => 'corporation',
-            'timestamp' => Carbon::now()->format('Y-m-d H:i:s'),
-            'is_read' => 0,
-            'text' => "firedBy: 2115784309\nfiredByLink: <a href=\"showinfo:1386//2115784309\">CJ Kondur</a>\nmoonID: 40204351\noreVolumeByType:\n  45494: 8364045.526322877\n  45498: 10867302.250553984\n  45512: 4960061.982821797\nsolarSystemID: 30003222\nstructureID: 1038565229449\nstructureLink: <a href=\"showinfo:35835//1038565229449\">Q-ITV5 - A38 P4M3</a>\nstructureName: Q-ITV5 - A38 P4M3\nstructureTypeID: 35835\n"
+            'timestamp' => Carbon::parse('2024-07-05T01:14:00Z')->format('Y-m-d H:i:s'),
+            'is_read' => true,
+            'text' => "allianceID: 99005338\nallianceLinkData:\n- showinfo\n- 16159\n- 99005338\nallianceName: Pandemic Horde\narmorPercentage: 100.0\ncharID: 2114464253\ncorpLinkData:\n- showinfo\n- 2\n- 2014367342\ncorpName: Blackwater USA Inc.\nhullPercentage: 100.0\nisActive: true\nitemID: &id001 1045848302693\nplanetID: 40206108\nplanetShowInfoData:\n- showinfo\n- 2015\n- 40206108\nshieldPercentage: 94.92754468794297\nskyhookShowInfoData:\n- showinfo\n- 81080\n- *id001\nsolarsystemID: 30003249\ntypeID: 81080\n"
         ]);
         $this->info('Notification created.');
     }
-
 }
